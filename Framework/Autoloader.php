85aca1f6f8f4b1bd26bee6febf04b8e18a0861c7
saysa <sbo@conamed.de>
2017-08-26 15:04:53 +0200

    add comments

--- Framework/Autoloader.php
+++ Framework/Autoloader.php
@@ -4,6 +4,13 @@ namespace Framework;
 
 class Autoloader {
 
+	/**
+	 * Includes Class file if exists
+	 *
+	 * @param $class
+	 *
+	 * @throws \Exception
+	 */
 	public static function autoload( $class ) {
 
 		if ( file_exists( $file = APP_PATH . DIRECTORY_SEPARATOR . $class . ".php" ) ) {
--- Framework/Core.php
+++ Framework/Core.php
@@ -4,6 +4,9 @@ namespace Framework;
 
 class Core {
 
+	/**
+	 * Implements autoload
+	 */
 	public static function initialize() {
 		include "Autoloader.php";
 		spl_autoload_register( array( "Framework\Autoloader", "autoload" ) );
