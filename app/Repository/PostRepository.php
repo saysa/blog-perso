c7f11744afb6cd22452d614618be05907c7ae13c
saysa <sbo@conamed.de>
2017-08-30 14:34:10 +0200

    create the repository class

---
+++ app/Repository/PostRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\Repository;
+
+use Framework\Entity;
+use Framework\Repository;
+
+class PostRepository extends Repository {
+
+	public function add( Entity $entity ) {
+		// TODO: Implement add() method.
+	}
+
+	public function delete( Entity $form ) {
+		// TODO: Implement delete() method.
+	}
+
+	public function update( Entity $form ) {
+		// TODO: Implement update() method.
+	}
+
+	public function get( $id ) {
+		// TODO: Implement get() method.
+	}
+
+	public function getList() {
+		// TODO: Implement getList() method.
+	}
+}
