62e70a558428e761ba2ce94c225714979ac734e7
saysa <sbo@conamed.de>
2017-08-30 08:29:04 +0200

    instanciate the View object in the Controller

--- Framework/Controller.php
+++ Framework/Controller.php
@@ -4,8 +4,11 @@ namespace Framework;
 
 class Controller {
 
+	protected $_view;
+
 	public function __construct() {
 
-		$router = Registry::get( "router" );
+		$router      = Registry::get( "router" );
+		$this->_view = new View();
 	}
 }
