c1de972a2779a2032201da96b5b66aa84cbf95d6
saysa <sbo@conamed.de>
2017-08-30 17:02:00 +0200

    add 'created' and 'modified' attributes for the Entities

--- Framework/Entity.php
+++ Framework/Entity.php
@@ -4,6 +4,9 @@ namespace Framework;
 
 class Entity {
 
+	protected $_created;
+	protected $_modified;
+
 	public function __construct( array $parameters ) {
 
 		$this->hydrate( $parameters );
@@ -23,5 +26,33 @@ class Entity {
 		}
 
 		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getCreated() {
+		return $this->_created;
+	}
+
+	/**
+	 * @param mixed $created
+	 */
+	public function setCreated( $created ) {
+		$this->_created = $created;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getModified() {
+		return $this->_modified;
+	}
+
+	/**
+	 * @param mixed $modified
+	 */
+	public function setModified( $modified ) {
+		$this->_modified = $modified;
 	}
 }
