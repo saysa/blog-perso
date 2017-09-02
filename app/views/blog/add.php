4f12c8407ddd5bc87a4d53e72f7355d4fb4578fa
saysa <sbo@conamed.de>
2017-09-02 09:49:01 +0200

    the add a post form is missing

---
+++ app/views/blog/add.php
@@ -0,0 +1,23 @@
+<h1>Ajouter un Post</h1>
+
+<form method="post" action="">
+    <div>
+        <label for="title">Titre :</label><br>
+        <input type="text" name="title" id="title">
+    </div>
+    <div>
+        <label for="lead">Chapô :</label><br>
+        <textarea name="lead" id="lead" cols="50" rows="4"></textarea>
+    </div>
+    <div>
+        <label for="content">Contenu :</label><br>
+        <textarea name="content" id="content" cols="50" rows="8"></textarea>
+    </div>
+    <div>
+        <label for="author">Auteur :</label><br>
+        <input type="text" name="author" id="author">
+    </div>
+    <div>
+        <input type="submit" name="submit" value="Ajouter un Post">
+    </div>
+</form>
