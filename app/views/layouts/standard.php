35098a10d5b61582c9600df1e13f4ccad34bda29
saysa <sbo@conamed.de>
2017-09-04 16:48:42 +0200

    set base_url as global view variable
    and add the link to the page : add post form

--- app/views/layouts/standard.php
+++ app/views/layouts/standard.php
@@ -5,6 +5,11 @@
     <title>Mon blog</title>
 </head>
 <body>
+<nav>
+    <ul>
+        <li><a href="<?= $data['base_url'] ?>blog/add">Add a Post</a></li>
+    </ul>
+</nav>
 <?php include $data['actionFile']; ?>
 </body>
 </html>
